fc81da761dea7920ca0782c0a57c9b17070569ea
disnkitty <chirka19982007@gmail.com>
2025-05-29 01:02:10 +0300

    added to class  Department properties

--- hrdepartment/Models/Department.cs
+++ hrdepartment/Models/Department.cs
@@ -8,5 +8,16 @@ namespace hrdepartment.Models
 {
     internal class Department
     {
+            public int Id { get; set; }                       
+            public string Name { get; set; }              
+            public string Manager { get; set; }    
+            public List<Employee> Employees { get; set; }    
+
+            public Department()
+            {
+                Employees = new List<Employee>();
+            }
+        }
     }
-}
+
+
