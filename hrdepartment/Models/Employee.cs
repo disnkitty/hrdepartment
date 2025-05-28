e66d33106738841b9070544e64dd07dbea884dbf
disnkitty <chirka19982007@gmail.com>
2025-05-28 14:10:40 +0300

    added to class Employee properties

--- hrdepartment/Models/Employee.cs
+++ hrdepartment/Models/Employee.cs
@@ -8,5 +8,18 @@ namespace hrdepartment.Models
 {
     internal class Employee
     {
+        public int Id { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string PassportData { get; set; }
+        public string Education { get; set; }
+        public string Specialty { get; set; }
+        public string Position { get; set; }
+        public string Salary { get; set; }
+        public string HireDate { get; set; }
+        public Department Department { get; set; }
+        public DateTime LastAppointmentDate { get; set; }
+
     }
 }
