99cf4054db9cbc5709a26dac60f8b7188e2f43d5
disnkitty <chirka19982007@gmail.com>
2025-05-31 22:03:54 +0300

    added new WinForms

--- hrdepartment/DepartmentForm.cs
+++ hrdepartment/DepartmentForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace hrdepartment
 {
-    public partial class Form1 : Form
+    public partial class DepartmentForm : Form
     {
-        public Form1()
+        public DepartmentForm()
         {
             InitializeComponent();
         }
