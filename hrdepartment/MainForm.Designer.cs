9c7d52b014ebe608e2b568048ef49360c4b8bcf4
disnkitty <chirka19982007@gmail.com>
2025-06-01 12:31:28 +0300

    made one of the forms

--- hrdepartment/MainForm.Designer.cs
+++ hrdepartment/MainForm.Designer.cs
@@ -28,13 +28,190 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.departmentComboBox = new System.Windows.Forms.ComboBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.searchBox = new System.Windows.Forms.TextBox();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.searchLabel = new System.Windows.Forms.Label();
+            this.departmentsLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FullName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Age = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Department = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Position = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FullInfo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // departmentComboBox
+            // 
+            this.departmentComboBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.departmentComboBox.FormattingEnabled = true;
+            this.departmentComboBox.Location = new System.Drawing.Point(115, 166);
+            this.departmentComboBox.Margin = new System.Windows.Forms.Padding(4);
+            this.departmentComboBox.Name = "departmentComboBox";
+            this.departmentComboBox.Size = new System.Drawing.Size(914, 37);
+            this.departmentComboBox.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(114, 278);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(914, 37);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // searchBox
+            // 
+            this.searchBox.Location = new System.Drawing.Point(114, 62);
+            this.searchBox.Name = "searchBox";
+            this.searchBox.Size = new System.Drawing.Size(765, 35);
+            this.searchBox.TabIndex = 2;
+            // 
+            // searchButton
+            // 
+            this.searchButton.BackColor = System.Drawing.Color.Indigo;
+            this.searchButton.ForeColor = System.Drawing.Color.White;
+            this.searchButton.Location = new System.Drawing.Point(915, 57);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(114, 40);
+            this.searchButton.TabIndex = 3;
+            this.searchButton.Text = "search";
+            this.searchButton.UseVisualStyleBackColor = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.FullName,
+            this.Age,
+            this.Department,
+            this.Position,
+            this.FullInfo});
+            this.dataGridView1.Location = new System.Drawing.Point(114, 361);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(984, 267);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // searchLabel
+            // 
+            this.searchLabel.AutoSize = true;
+            this.searchLabel.Location = new System.Drawing.Point(110, 20);
+            this.searchLabel.Name = "searchLabel";
+            this.searchLabel.Size = new System.Drawing.Size(251, 29);
+            this.searchLabel.TabIndex = 5;
+            this.searchLabel.Text = "Search by last name:";
+            // 
+            // departmentsLabel
+            // 
+            this.departmentsLabel.AutoSize = true;
+            this.departmentsLabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(8)))));
+            this.departmentsLabel.Location = new System.Drawing.Point(110, 169);
+            this.departmentsLabel.Name = "departmentsLabel";
+            this.departmentsLabel.Size = new System.Drawing.Size(165, 29);
+            this.departmentsLabel.TabIndex = 6;
+            this.departmentsLabel.Text = "Departments:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(8)))));
+            this.label1.Location = new System.Drawing.Point(110, 281);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 29);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Orders:";
+            // 
+            // id
+            // 
+            this.id.HeaderText = "id";
+            this.id.MinimumWidth = 8;
+            this.id.Name = "id";
+            this.id.Width = 150;
+            // 
+            // FullName
+            // 
+            this.FullName.HeaderText = "Full Name";
+            this.FullName.MinimumWidth = 8;
+            this.FullName.Name = "FullName";
+            this.FullName.Width = 175;
+            // 
+            // Age
+            // 
+            this.Age.HeaderText = "Age";
+            this.Age.MinimumWidth = 8;
+            this.Age.Name = "Age";
+            this.Age.Width = 150;
+            // 
+            // Department
+            // 
+            this.Department.HeaderText = "Department";
+            this.Department.MinimumWidth = 8;
+            this.Department.Name = "Department";
+            this.Department.Width = 150;
+            // 
+            // Position
+            // 
+            this.Position.HeaderText = "Position";
+            this.Position.MinimumWidth = 8;
+            this.Position.Name = "Position";
+            this.Position.Width = 150;
+            // 
+            // FullInfo
+            // 
+            this.FullInfo.HeaderText = "Full Info";
+            this.FullInfo.MinimumWidth = 8;
+            this.FullInfo.Name = "FullInfo";
+            this.FullInfo.Width = 150;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 29F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(1142, 731);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.departmentsLabel);
+            this.Controls.Add(this.searchLabel);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.searchBox);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.departmentComboBox);
+            this.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "MainForm";
+            this.Text = "ГОЛОВНА СТОРІНКА";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox departmentComboBox;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox searchBox;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label searchLabel;
+        private System.Windows.Forms.Label departmentsLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FullName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Age;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Department;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Position;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FullInfo;
     }
 }
 
--- hrdepartment/MainForm.cs
+++ hrdepartment/MainForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using hrdepartment.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,31 @@ namespace hrdepartment
 {
     public partial class MainForm : Form
     {
+        private Company company;
+
         public MainForm()
         {
             InitializeComponent();
+            company = new Company();
+
+            company.AddDepartment(new Department { Id = 1, Name = "HR" });
+            company.AddDepartment(new Department { Id = 2, Name = "IT" });
+            company.AddDepartment(new Department { Id = 3, Name = "Sales" });
+
+            this.Load += MainForm_Load;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            departmentComboBox.Items.Clear();
+            foreach (var dept in company.Departments)
+            {
+                departmentComboBox.Items.Add(dept.Name);
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
         }
     }
 }
